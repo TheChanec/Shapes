23c97e38bf265deedacf140cd875cd0a3c60752b
Enrique Chan <enrique.nec@gmail.com>
2019-07-12 15:11:44 -0500

    Update Program.cs

--- ShapeConsoleApp/Program.cs
+++ ShapeConsoleApp/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ShapeConsoleApp.Models;
+using System;
+using System.Collections.Generic;
 
 namespace ShapeConsoleApp
 {
@@ -6,7 +8,15 @@ namespace ShapeConsoleApp
     {
         static void Main(string[] args)
         {
+            List<Shape> Shapes = new List<Shape>();
+            Shapes.Add(new Triangle(Color.Yellow,5,6));
+            Shapes.Add(new Square(Color.Red, 8));
+            Shapes.Add(new Circle(Color.Blue, 5));
+            Shapes.Add(new Triangle(Color.Green, 5, 6));
+            Shapes.Add(new Rectangle(Color.Purple,10,5));
+
             Console.WriteLine("Hello World!");
+
         }
     }
 }
