c100e5d8474699f061dcd094e00f2aefe5baacda
Enrique Chan <enrique.nec@gmail.com>
2019-07-12 12:46:17 -0500

    Create Color.cs

---
+++ ShapeConsoleApp/Models/Color.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShapeConsoleApp.Models
+{
+    public enum Color
+    {
+        Blue,
+        Green,
+        Red,
+        Purple,
+        Yellow
+    }
+}
