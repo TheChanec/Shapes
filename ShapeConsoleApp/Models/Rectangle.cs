8e03653cd13088ec3dd93fde5c794558c3b841f8
Enrique Chan <enrique.nec@gmail.com>
2019-07-12 15:11:34 -0500

    Update Rectangle.cs

--- ShapeConsoleApp/Models/Rectangle.cs
+++ ShapeConsoleApp/Models/Rectangle.cs
@@ -1,10 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace ShapeConsoleApp.Models
+﻿namespace ShapeConsoleApp.Models
 {
-    class Rectangle
+    public class Rectangle : Shape
     {
+        private readonly int _height;
+        private readonly int _width;
+
+        public Rectangle(Color color, int _height, int width) : base(color)
+        {
+            _height = _height;
+            _width = width;
+        }
+
+        public override double Area
+        {
+            get
+            {
+                return _width * _height;
+            }
+        }
     }
 }
