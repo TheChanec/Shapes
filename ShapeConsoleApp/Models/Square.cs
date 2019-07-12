8b36574cd6e1c77a4674ed437113204bee73ccce
Enrique Chan <enrique.nec@gmail.com>
2019-07-12 12:41:30 -0500

    create models base

---
+++ ShapeConsoleApp/Models/Square.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShapeConsoleApp.Models
+{
+    class Square
+    {
+    }
+}
---
+++ ShapeConsoleApp/Models/Triangle.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShapeConsoleApp.Models
+{
+    public class Triangle : Shape
+    {
+    }
+}
