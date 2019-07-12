21cb22706b4c82295ffac9a04516e804b2db72fa
Enrique Chan <enrique.nec@gmail.com>
2019-07-12 15:11:42 -0500

    Update Triangle.cs

--- ShapeConsoleApp/Models/Triangle.cs
+++ ShapeConsoleApp/Models/Triangle.cs
@@ -1,10 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace ShapeConsoleApp.Models
+﻿namespace ShapeConsoleApp.Models
 {
     public class Triangle : Shape
     {
+        private readonly int _height;
+        private readonly int _base;
+
+        public Triangle(Color color, int height, int base_) : base(color)
+        {
+            _height = height;
+            _base = base_;
+        }
+
+        public override double Area { get => (_base * _height / 2) ; }
     }
 }
